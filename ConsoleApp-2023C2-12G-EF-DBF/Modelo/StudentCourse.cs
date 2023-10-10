3cccc99733494767191ef127844412a981657b2f
christian.slapak <christian.slapak@ort.edu.ar>
2023-10-09 21:52:35 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp-2023C2-12G-EF-DBF/Modelo/StudentCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_2023C2_12G_EF_DBF.Modelo
+{
+    public partial class StudentCourse
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ ConsoleApp-2023C2-12G-EF-DBF/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp_2023C2_12G_EF_DBF.Modelo;
+using System;
+using System.Linq;
+
+namespace ConsoleApp_2023C2_12G_EF_DBF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new SchoolDBContext();
+
+            var misCursos = context.Course.ToList();
+
+            foreach (var item in misCursos)
+            {
+                Console.WriteLine("Nombre curso:" + item.CourseName + " Id:" + item.CourseId);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
